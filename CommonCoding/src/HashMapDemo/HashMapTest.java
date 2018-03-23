3db31a85bbb29c925ddec4e08da7d2db752a4f35
lang <1098287206@qq,com>
2018-03-23 16:47:37 +0800

    初始化提交

---
+++ CommonCoding/src/HashMapDemo/HashMapTest.java
@@ -0,0 +1,20 @@
+package HashMapDemo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by lang on 2018/3/23.
+ */
+public class HashMapTest {
+
+    public static void main(String[] args){
+        Map<String, Long> map = new ConcurrentHashMap<>();
+        System.out.println(  ((-121>> 31) ^ (131 >> 31)) == 0);
+        System.out.println(0^0);
+        System.out.println(123^0);
+    }
+
+
+}
